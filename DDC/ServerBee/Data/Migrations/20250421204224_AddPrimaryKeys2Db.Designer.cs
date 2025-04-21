6a28e391cad47ff517e842c5f8b0b856c52eb0c2
csk2q <143836825+csk2q@users.noreply.github.com>
2025-04-21 15:45:09 -0500

    Add migration for marking primary keys

--- DDC/ServerBee/Data/Migrations/20250421204224_AddPrimaryKeys2Db.Designer.cs
+++ DDC/ServerBee/Data/Migrations/20250421204224_AddPrimaryKeys2Db.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ServerBee.Data;
@@ -11,9 +12,11 @@ using ServerBee.Data;
 namespace ServerBee.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250421204224_AddPrimaryKeys2Db")]
+    partial class AddPrimaryKeys2Db
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
