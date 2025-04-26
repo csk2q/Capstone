bd9f24b5c651cc33281b8da2c5415b180e057bd3
abotts-umkc <143641443+abotts-umkc@users.noreply.github.com>
2025-04-26 14:17:58 -0500

    adding builder service & line to controller for AI bot

--- DDC/ServerBee/Program.cs
+++ DDC/ServerBee/Program.cs
@@ -12,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // Add services to the container.
+builder.Services.AddAuthorization();
+
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 builder.Services.AddHttpClient();
 
