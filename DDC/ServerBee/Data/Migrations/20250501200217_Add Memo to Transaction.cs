8a6dd5dc3c6c404dcdfbf45d3c724353021d4b8f
csk2q <143836825+csk2q@users.noreply.github.com>
2025-05-01 15:03:18 -0500

    Add Memo field to Transaction

---
+++ DDC/ServerBee/Data/Migrations/20250501200217_Add Memo to Transaction.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerBee.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMemotoTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "memo",
+                table: "transaction",
+                type: "character varying(250)",
+                maxLength: 250,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "memo",
+                table: "transaction");
+        }
+    }
+}
